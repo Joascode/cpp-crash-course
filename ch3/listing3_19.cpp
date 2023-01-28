369bfb143e44b9b373515c417370bfd4022af93d
Joas Kramer <joaskramer@outlook.com>
2023-01-28 22:41:37 +0100

    Adds listing3_19
    Last listing of the chapter. This conclused the chapter about pointers, references, const and auto.

---
+++ ch3/listing3_19.cpp
@@ -0,0 +1,29 @@
+/*
+  This is for example purposes only!
+*/
+
+// Example of auto deducing the correct type based on initializer.
+// VS Code apparently also helps by showing the used type.
+// PS. Auto formatting messing up the comments behind the types.
+auto the_answer{42};           // int
+auto foot{12L};                // long
+auto rootbeer{5.0F};           // float
+auto cheeseburger{10.0};       // double
+auto politifact_claims{false}; // bool
+auto cheese{"string"};         // const char * (char[7])
+
+struct Dwarf {};
+
+Dwarf dwarves[13]; // NOLINT
+
+struct Contract {
+  // Useless method for example purposes.
+  void Add(const Dwarf &);
+};
+
+void FormCompany(Contract &contract) {
+  // Use of auto inside a range loop.
+  for (const auto &dwarf : dwarves) {
+    contract.Add(dwarf);
+  }
+}
