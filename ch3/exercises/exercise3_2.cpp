915bd22aee4a854c413c05bb81cac3f2d09cd1ff
Joas Kramer <joaskramer@outlook.com>
2023-01-28 23:48:38 +0100

    Adds upper bound checks for WriteTo and ReadFrom

--- ch3/exercises/exercise3_2.cpp
+++ ch3/exercises/exercise3_2.cpp
@@ -1,8 +1,17 @@
 #include <cstdio>
 
-void WriteTo(char *word, int index, char letter) { word[index] = letter; }
+void WriteTo(char *word, size_t n_letters, int index, char letter) {
+  if (index < n_letters - 1) {
+    word[index] = letter;
+  }
+}
 
-auto ReadFrom(const char *word, int index) -> char { return word[index]; }
+auto ReadFrom(const char *word, size_t n_letters, int index) -> char {
+  if (index < n_letters - 1) {
+    return word[index];
+  }
+  return word[0];
+}
 
 auto main() -> int {
   char lower[] = "abc?e";  // NOLINT
@@ -11,11 +20,13 @@ auto main() -> int {
 
   // The mechanism is the same for changing the 4th element in the array
   // even though upper_ptr is a pointer to the first element of the array
-  WriteTo(lower, 3, 'd');
-  WriteTo(upper_ptr, 3, 'D');
+  WriteTo(lower, sizeof(lower) / sizeof(lower[0]), 3, 'd');
+  WriteTo(upper_ptr, sizeof(upper) / sizeof(upper[0]), 3, 'D');
 
-  const char letter_d = ReadFrom(lower, 3);
-  const char letter_D = ReadFrom(upper_ptr, 3); // NOLINT
+  const char letter_d = ReadFrom(lower, sizeof(lower) / sizeof(lower[0]), 3);
+  const char letter_D = // NOLINT
+      ReadFrom(upper_ptr, sizeof(upper) / sizeof(upper[0]), 6);
 
   printf("lower: %s\nupper: %s\n", lower, upper);
+  printf("lower d: %c\nupper D: %c\n", letter_d, letter_D);
 }
