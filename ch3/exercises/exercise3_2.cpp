d0f7425bc95b30d3daf5f902b64c279a31d8f8be
Joas Kramer <joaskramer@outlook.com>
2023-01-28 23:07:03 +0100

    Adds ReadFrom function
    Changes direct array access with new ReadFrom function.

---
+++ ch3/exercises/exercise3_2.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+
+auto ReadFrom(const char *word, int index) -> char { return word[index]; }
+
+auto main() -> int {
+  char lower[] = "abc?e";  // NOLINT
+  char upper[] = "ABC?E";  // NOLINT
+  char *upper_ptr = upper; // Equivalent to &upper[0]
+
+  // The mechanism is the same for changing the 4th element in the array
+  // even though upper_ptr is a pointer to the first element of the array
+  lower[3] = 'd';
+  upper_ptr[3] = 'D';
+
+  const char letter_d = ReadFrom(lower, 3);
+  const char letter_D = ReadFrom(upper_ptr, 3); // NOLINT
+
+  printf("lower: %s\nupper: %s\n", lower, upper);
+}
