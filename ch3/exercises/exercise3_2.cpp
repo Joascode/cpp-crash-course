b780b072ce6a07c3ce96a184b04bbe6e26f7024b
Joas Kramer <joaskramer@outlook.com>
2023-01-28 23:09:51 +0100

    Adds WriteTo function
    This function writes to a given char array based on index and letter given. This replaces
    the direct array access and hides the functionality of writing new letters into the array.

--- ch3/exercises/exercise3_2.cpp
+++ ch3/exercises/exercise3_2.cpp
@@ -1,5 +1,7 @@
 #include <cstdio>
 
+void WriteTo(char *word, int index, char letter) { word[index] = letter; }
+
 auto ReadFrom(const char *word, int index) -> char { return word[index]; }
 
 auto main() -> int {
@@ -9,8 +11,8 @@ auto main() -> int {
 
   // The mechanism is the same for changing the 4th element in the array
   // even though upper_ptr is a pointer to the first element of the array
-  lower[3] = 'd';
-  upper_ptr[3] = 'D';
+  WriteTo(lower, 3, 'd');
+  WriteTo(upper_ptr, 3, 'D');
 
   const char letter_d = ReadFrom(lower, 3);
   const char letter_D = ReadFrom(upper_ptr, 3); // NOLINT
