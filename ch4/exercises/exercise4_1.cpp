280bc69b6ec3a81d7f85308e0016228e9923d3aa
Joas Kramer <joaskramer@outlook.com>
2023-02-21 13:40:12 +0100

    Adds destructor which prints age of TimerClass

--- ch4/exercises/exercise4_1.cpp
+++ ch4/exercises/exercise4_1.cpp
@@ -1,7 +1,13 @@
+#include <cstdio>
 #include <ctime>
 
 struct TimerClass {
   TimerClass() : timestamp{std::time(nullptr)} {}
+
+  ~TimerClass() {
+    const std::time_t age = std::time(nullptr) - timestamp;
+    printf("Age of TimerClass: %ld\n", age);
+  }
 
   std::time_t timestamp;
 };
