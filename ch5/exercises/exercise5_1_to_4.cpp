d991171f46981da0b911af5b885c645bb39cbdec
Joas Kramer <joaskramer@outlook.com>
2023-02-21 23:17:31 +0100

    Adds AccountDatabase reference to Bank (exercise 5-3)
    This was actually a bit challenging, figuring out how to use arrays of user-defined types. It works, but since
    I'm not an expert in this I have this constant feeling that I'm missing something or leaking memory :c
    
    In all honesty, this isn't the cleanest of code in a long shot. I seriously despise the MakeTransfer method atm, but
    it works and I kind of want to get going with the next exercise.

--- ch5/exercises/exercise5_1_to_4.cpp
+++ ch5/exercises/exercise5_1_to_4.cpp
@@ -30,10 +30,15 @@ struct Account {
   double amount;
 };
 
+struct Transfer {
+  const long accountId;
+  const double amount;
+};
+
 struct AccountDatabase {
   virtual ~AccountDatabase() = default;
   virtual auto RetrieveAmount(long accountId) -> double = 0;
-  virtual void SetAmount(long accountId, double amount) = 0;
+  virtual void SetAmount(Transfer transfer) = 0;
 };
 
 struct InMemoryAccountDatabase : AccountDatabase {
@@ -50,28 +55,43 @@ struct InMemoryAccountDatabase : AccountDatabase {
                       amount = account.amount;
                     }
                   });
+    printf("Retrieved %f from %ld\n", amount, accountId);
     return amount;
   }
 
-  void SetAmount(const long accountId, const double amount) override {
-    std::for_each(accounts_.cbegin(), accounts_.cend(),
-                  [&accountId, &amount](Account account) {
-                    if (account.id == accountId) {
-                      account.amount = amount;
-                    }
-                  });
+  void SetAmount(const Transfer transfer) override {
+    for (auto &account : accounts_) {
+      if (account.id == transfer.accountId) {
+        printf("Old amount was: %f\n", account.amount);
+        account.amount = transfer.amount;
+        printf("New amount is: %f\n", account.amount);
+      }
+    }
   }
 
 private:
+  // I'm assuming here that std::array doesn't need cleaning up, since it
+  // doesn't use new?
   std::array<Account, 2> accounts_;
 };
 
 struct Bank {
-  explicit Bank(Logger &logger) : logger_{logger} {}
-  void MakeTransfer(TransferDetails details) { logger_.LogTransfer(details); }
+  explicit Bank(Logger &logger, AccountDatabase &accountDB)
+      : logger_{logger}, accountDB_{accountDB} {}
+  void MakeTransfer(TransferDetails details) {
+    const double amount = accountDB_.RetrieveAmount(details.fromAccount);
+    if (amount != 0.0 || (amount - details.amount) > 0.0) {
+      accountDB_.SetAmount(
+          {details.toAccount,
+           (accountDB_.RetrieveAmount(details.toAccount) + details.amount)});
+      accountDB_.SetAmount({details.fromAccount, amount - details.amount});
+      logger_.LogTransfer(details);
+    }
+  }
 
 private:
   Logger &logger_;
+  AccountDatabase &accountDB_;
 };
 
 /**
@@ -95,8 +115,10 @@ private:
 
 auto main() -> int {
   ConsoleLogger logger;
-  Bank bank{logger};
+  InMemoryAccountDatabase account_db{Account{1000, 50.00},
+                                     Account{2000, 50.00}};
+  Bank bank{logger, account_db};
   // Bank bank{&logger};
   bank.MakeTransfer({1000, 2000, 49.95});
-  bank.MakeTransfer({2000, 4000, 20.00});
+  bank.MakeTransfer({2000, 1000, 20.00});
 }
