bb03d7ab37e93b8a1b23544bdc20281e40c210f7
Joas Kramer <joaskramer@outlook.com>
2023-02-21 14:15:28 +0100

    Fixes move assignment operator
    Also adds experimentation in main to see what happens.

--- ch4/exercises/exercise4_1.cpp
+++ ch4/exercises/exercise4_1.cpp
@@ -1,10 +1,14 @@
+#include <cstddef>
 #include <cstdio>
 #include <ctime>
+#include <utility>
 
 struct TimerClass {
-  TimerClass() : timestamp{std::time(nullptr)} {}
+  explicit TimerClass(const char *name)
+      : timestamp{std::time(nullptr)}, name{name} {}
   TimerClass(const TimerClass &other) = default;
   TimerClass(TimerClass &&other) noexcept : timestamp{other.timestamp} {
+    printf("Move constructor\n");
     other.timestamp = 0;
   }
 
@@ -16,22 +20,33 @@ struct TimerClass {
     return *this;
   }
   auto operator=(TimerClass &&other) noexcept -> TimerClass & {
+    printf("Move assignment\n");
     if (this == &other) {
       return *this;
     }
     timestamp = other.timestamp;
+    name = other.name;
+    other.name = nullptr;
     other.timestamp = 0;
     return *this;
   }
 
   ~TimerClass() {
+    printf("Timestamp: %ld\n", timestamp);
     if (timestamp != 0) {
       const std::time_t age = std::time(nullptr) - timestamp;
-      printf("Age of TimerClass: %ld\n", age);
+      printf("Age of %s: %ld\n", name, age);
+    } else {
+      printf("%s is moved.\n", name);
     }
   }
 
   std::time_t timestamp;
+  const char *name;
 };
 
-auto main() -> int { const TimerClass timer{}; }
+auto main() -> int {
+  TimerClass timer{"first timer"};
+  TimerClass timer2{"second timer"};
+  timer2 = std::move(timer);
+}
